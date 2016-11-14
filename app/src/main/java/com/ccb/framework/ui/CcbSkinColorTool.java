c8c54c7ab03c64b6fcccfbb97b0baedf519067cf
hjt <619625817@qq.com>
2016-11-14 17:57:08 +0800

    第一次提交版本--贺建涛办公

---
+++ app/src/main/java/com/ccb/framework/ui/CcbSkinColorTool.java
@@ -0,0 +1,11 @@
+package com.ccb.framework.ui;
+
+/**
+ * Created by CCB on 2016/11/14.
+ */
+
+public class CcbSkinColorTool {
+    private static final int COLOR_NULL=0;
+    public static final float DARK_OFFSET=0.611073F;
+    public static final String GENERAL_SKIN_COLOR_KEY="general_skin_color";
+}
